357d42e0515d5004f18a1ecb5190b31d83d828a2
Darius Stewart <darius.stewart31@gmail.com>
2020-01-13 04:43:58 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -31,7 +31,7 @@ def third_challenge
         age: "50"
       },
       :matriarch => {
-        name: "Lady Capulet"
+        name: "Lady Capulet",
         age: "51"
         },
       :heroine => {
