5b9995703a07e1f0c1fb491f9814a2f4642f7751
Darius Stewart <darius.stewart31@gmail.com>
2020-01-13 04:15:50 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,25 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
-      }, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => [
+        {
+        name: "Benvolio",
+        age: "17",
+        attitude: "worried"
+      },
+      {
+        name: "Mercutio"
+        age: "18"
+        attitude: "hot-headed"
+      }
+      ]
+      },
     :capulet => {
       #your key/value pairs here!
       }}
 
-  
+
 end
